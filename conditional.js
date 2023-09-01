bd8b7b1bb760132ac84b6ffbf74ab8e3647f5f65
Nishtha Patel <nishtha.patel@sky.uk>
2023-09-01 12:05:14 +0100

    conditional exercise complete

---
+++ conditional.js
@@ -0,0 +1,13 @@
+let age = 50;
+
+if (age <= 17) {
+    console.log("Underage");
+} else if (18 <= age && age <= 65) {
+    console.log("between 18 and 65");
+}else {
+    console.log("out of range")
+}
+
+
+// let result = (age>=18)? "underage": "legal" 
+// console.log(result)
