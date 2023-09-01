8026a59654300ad518ce47a8b14dff9f38594cbd
Nishtha Patel <nishtha.patel@sky.uk>
2023-09-01 13:51:43 +0100

    loops exercise complete

---
+++ JavaScript/loops.js
@@ -0,0 +1,23 @@
+let i = 100
+
+for (i; i<=10; i++) {
+    console.log(i);
+}
+
+
+let j = 10
+
+for (j; j>=0; j--) {
+    console.log(j);
+}
+
+let x = 2
+let loopCounter = 0
+
+while (x<10000) {
+    x = x**2
+    console.log(x)
+    loopCounter++
+    console.log(loopCounter)
+}
+
